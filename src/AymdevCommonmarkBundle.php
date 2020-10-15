4181b64e3c342e5a5079954b63f42f42e83b463b
AymDev <aymericmayeux@gmail.com>
2020-10-15 23:27:31 +0200

    ADD code coverage in CI

--- src/AymdevCommonmarkBundle.php
+++ src/AymdevCommonmarkBundle.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AymdevCommonmarkBundle extends Bundle
 {
+    /**
+     * @codeCoverageIgnore
+     */
     public function build(ContainerBuilder $container)
     {
         parent::build($container);

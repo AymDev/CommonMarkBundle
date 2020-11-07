39f3ce70bdb9ff8c0b8226307cebf4bb92d4af3a
AymDev <aymericmayeux@gmail.com>
2020-11-07 17:37:45 +0100

    REMOVED the services.xml
    Replaced the services.xml service definitions by manual definitions
    in the bundle extension class.

--- src/DependencyInjection/AymdevCommonmarkExtension.php
+++ src/DependencyInjection/AymdevCommonmarkExtension.php
@@ -3,8 +3,14 @@
 namespace Aymdev\CommonmarkBundle\DependencyInjection;
 
 use Aymdev\CommonmarkBundle\DependencyInjection\Compiler\ConvertersPass;
+use Aymdev\CommonmarkBundle\Twig\CommonMarkExtension;
+use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\ConfigurableEnvironmentInterface;
+use League\CommonMark\Environment;
+use League\CommonMark\GithubFlavoredMarkdownConverter;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
@@ -15,11 +21,46 @@ class AymdevCommonmarkExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $this->createBundleServiceDefinitions($container);
 
         // Save config in container
         // Validates in compiler pass
         $container->setParameter(ConvertersPass::PARAMETER_CONVERTERS, $config['converters']);
+    }
+
+    /**
+     * Create service definitions for the bundle
+     */
+    private function createBundleServiceDefinitions(ContainerBuilder $container): void
+    {
+        // Environment
+        $container
+            ->setDefinition('aymdev_commonmark.environment', new Definition(ConfigurableEnvironmentInterface::class))
+            ->setFactory([Environment::class, 'createCommonMarkEnvironment'])
+            ->setPublic(false)
+        ;
+
+        // Converters
+        $container
+            ->setDefinition('aymdev_commonmark.converter.type.commonmark', new Definition(CommonMarkConverter::class))
+            ->setPublic(false)
+        ;
+
+        $container
+            ->setDefinition('aymdev_commonmark.converter.type.github', new Definition(GithubFlavoredMarkdownConverter::class))
+            ->setPublic(false)
+        ;
+
+        $container
+            ->setAlias('aymdev_commonmark.converter.type.empty', 'aymdev_commonmark.converter.type.commonmark')
+            ->setPublic(false)
+        ;
+
+        // Twig extension
+        $container
+            ->setDefinition('aymdev_commonmark.twig_extension', new Definition(CommonMarkExtension::class))
+            ->addTag('twig.extension')
+            ->setPublic(false)
+        ;
     }
 }
